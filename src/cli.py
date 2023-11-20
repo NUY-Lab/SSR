7d42a96063be3ff5555ce87b6c35c01c0ff1e9d3
tenjo-t <tenjo0314takashi@gmail.com>
2023-11-20 14:49:13 +0900

    ScriptフォルダをPathに追加する処理を削除

--- src/cli.py
+++ src/cli.py
@@ -1,11 +1,3 @@
-import sys
-from pathlib import Path
-
-# hack
-cwd = Path.cwd()
-sys.path.append(str(cwd))
-sys.path.append(str(cwd / "scripts"))
-
 from cli.main import main
 
 if __name__ == "__main__":
